99953b7452db12d1f73fb0e0dc9470ec3f322cf5
Yan Carlos Cubas Quispe <yancarlos180203@gmail.com>
2025-10-21 15:49:33 -0500

    Initial commit

---
+++ src/main/java/com/novahabitat/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.novahabitat.service;
+
+import com.novahabitat.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService extends IGenericService<Customer, Integer>{
+    //Customer validAndSave(Customer customer);
+    /*Customer save(Customer customer) throws  Exception;
+    Customer update(Customer customer, Integer id) throws  Exception;
+    List<Customer> findAll() throws  Exception;
+    Customer findById(Integer id) throws  Exception;
+    void delete(Integer id) throws  Exception;
+*/
+}
